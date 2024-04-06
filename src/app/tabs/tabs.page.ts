d271c725c7be8d01f016543c6fbfd6cc1a622f00
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-04-06 11:30:35 +0800

    add 4th page

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,10 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-
   constructor() {}
 
+  tabs = [
+    {
+      title: 'Tab 1',
+      icon: 'home',
+      link: '/tabs/tab1',
+    },
+    {
+      title: 'Tab 2',
+      icon: 'list',
+      link: '/tabs/tab2',
+    },
+    {
+      title: 'Tab 3',
+      icon: 'options',
+      link: '/tabs/tab3',
+    },
+    {
+      title: 'Tab 4',
+      icon: 'bookmark',
+      link: '/tabs/tab4',
+    },
+  ];
 }
